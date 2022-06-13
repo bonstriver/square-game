79d607cd929705071b03066fbb41ceedf876da8d
bonstriver <brandon.martinez0502@gmail.com>
2022-06-12 17:25:03 -0700

    X's and O's

--- src/index.js
+++ src/index.js
@@ -2,29 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const Square=(props)=>{
-      return (
+  // ========================================
+  
+const Square=(props)=>(
+      // {return(...)}
         <button 
-        className="square" 
-        onClick={()=> props.onClick({})}
+          className="square" 
+          onClick={props.onClick}
         >
           {props.value}
         </button>
       );
-  }
   
 class Board extends React.Component {
     constructor(props){
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i){
       const squares = this.state.squares.slice()
-      squares[i] = 'X'
-      this.setState({squares: squares})
+      squares[i] = this.state.xIsNext ? 'X' : 'O'
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
     }
   
     renderSquare(i) {
@@ -37,7 +42,9 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = `Next player: ${
+        this.state.xIsNext ? 'X' : 'O'
+      }`;
   
       return (
         <div>
@@ -82,4 +89,3 @@ class Game extends React.Component {
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-  
